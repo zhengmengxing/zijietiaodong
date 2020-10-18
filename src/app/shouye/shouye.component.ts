03c7f49e9037081fb1bdbc6bb43836bed5034c39
郑梦醒 <zhengmengxing@mengxing.local>
2020-10-18 11:20:05 +0800

    第一次上传

---
+++ src/app/shouye/shouye.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shouye',
+  templateUrl: './shouye.component.html',
+  styleUrls: ['./shouye.component.css']
+})
+export class ShouyeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
